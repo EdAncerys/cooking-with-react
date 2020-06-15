22c7f646006345011930ff7a906b12b6dc566b79
Ed <lookatemail@gmail.com>
2020-06-15 12:13:24 +0100

    add recipe list

--- src/components/Recepe.jsx
+++ src/components/Recepe.jsx
@@ -1,5 +1,31 @@
 import React from "react";
 
 export default function Recepe() {
-  return <>"Hello World"</>;
+  return (
+    <div>
+      <div>
+        <h3>Plain Chicken</h3>
+        <div>
+          <button>Edit</button>
+          <button>Delete</button>
+        </div>
+      </div>
+      <div>
+        <span>Cook Time</span>
+        <span>1:45</span>
+      </div>
+      <div>
+        <span>Servings</span>
+        <span>3</span>
+      </div>
+      <div>
+        <span>Instructions</span>
+        <div>
+          <p>Put salt on chicken</p>
+          <p>Put chicken in oven</p>
+          <p>Eat chicken</p>
+        </div>
+      </div>
+    </div>
+  );
 }
